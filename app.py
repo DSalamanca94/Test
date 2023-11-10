35b8ee837eb9dbac1e55584d645a72b59fe680d4
David salamanca <d.salamancaz@uniandes.edu.co>
2023-11-09 21:37:53 -0500

    Last test

--- app.py
+++ app.py
@@ -18,22 +18,22 @@ class Status(Resource):
 class CreateFile(Resource):
     def post(self):
         # Nombre del archivo que deseas copiar
-        archivo_a_copiar = 'nombre_del_archivo.txt'
+        archivo_a_copiar = 'Test.txt'
 
         # Rutas completas de origen y destino
-        ruta_origen = 'gs://app-storage-folder/in/{}'.format(archivo_a_copiar)
-        ruta_destino = 'gs://app-storage-folder/out/{}'.format(archivo_a_copiar)
+        ruta_origen = 'gs://app-storage-folder/Input/{}'.format(archivo_a_copiar)
+        ruta_destino = 'gs://app-storage-folder/Output/{}'.format(archivo_a_copiar)
 
         try:
             storage_client = storage.Client()
             bucket = storage_client.get_bucket('app-storage-folder')
 
             # Comprobar si el archivo de origen existe
-            blob_origen = bucket.blob('in/{}'.format(archivo_a_copiar))
+            blob_origen = bucket.blob('Input/{}'.format(archivo_a_copiar))
             blob_origen.reload()
 
             # Copiar el archivo
-            blob_destino = bucket.blob('out/{}'.format(archivo_a_copiar))
+            blob_destino = bucket.blob('Output/{}'.format(archivo_a_copiar))
             bucket.copy_blob(blob_origen, blob_destino)
 
             return {'status': 'success', 'message': 'Archivo copiado correctamente.'}
